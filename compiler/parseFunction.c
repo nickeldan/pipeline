ce4098e55b7a176e23c2af46b07da9113be3a089
nickeldan <nickeldan@gmail.com>
2021-05-31 12:58:22 -0400

    committing some changes so I can analyze them on my Linux machine

--- compiler/parseFunction.c
+++ compiler/parseFunction.c
@@ -2,12 +2,130 @@
 
 #include "parserInternal.h"
 
+static int
+parseArgList(plLexicalScanner *scanner, int function_marker, plAstNode **arg_list)
+{
+    int ret;
+    plLexicalToken token;
+    plAstNode *name_node;
+
+    while (true) {
+        int previous_marker = scanner->last_marker;
+        plLexicalLocation location, comma_location;
+        plAstNode *arg_node, *type_node;
+
+        ret = NEXT_TOKEN(scanner, &token);
+        if (ret != PL_RET_OK) {
+            return ret;
+        }
+
+        if (token.marker == PL_MARKER_RIGHT_PARENS) {
+            if (previous_marker == PL_MARKER_COMMA) {
+                PARSER_ERROR("Unexpected ')' following ',' in argument list.");
+                return PL_RET_BAD_DATA;
+            }
+            break;
+        }
+
+        if (token.marker == PL_MARKER_COMMA) {
+            if (previous_marker == PL_MARKER_LEFT_PARENS || previous_marker == PL_MARKER_COMMA) {
+                PARSER_ERROR("Unexpected ',' following %s in argument list.",
+                             plLexicalMarkerName(previous_marker));
+                return PL_RET_BAD_DATA;
+            }
+
+            if (function_marker != PL_MARKER_SOURCE) {
+                PARSER_ERROR("Multiple arguments are only allowed in a SOURCE.");
+                return PL_RET_BAD_DATA;
+            }
+
+            memcpy(&comma_location, &token.location, sizeof(token.location));
+
+            continue;
+        }
+
+        if (token.marker != PL_MARKER_NAME) {
+            PARSER_ERROR("Unexpected %s following %s in argunent list.", plLexicalMarkerName(token.marker),
+                         plLexicalMarkerName(previous_marker));
+            plTokenCleanup(&token, scanner->table);
+            return PL_RET_BAD_DATA;
+        }
+
+        name_node = plAstNew(PL_MARKER_NAME);
+        if (!name_node) {
+            plTokenCleanup(&token, scanner->table);
+            return PL_RET_OUT_OF_MEMORY;
+        }
+        memcpy(&name_node->token, &token, sizeof(token));
+
+        ret = EXPECT_MARKER(scanner, PL_MARKER_COLON, &location);
+        if (ret != PL_RET_OK) {
+            goto cleanup_name_node;
+        }
+
+        ret = parseExtendedType(scanner, &type_node);
+        if (ret != PL_RET_OK) {
+            goto cleanup_name_node;
+        }
+
+        if (type_node->token.marker != PL_MARKER_TYPE) {
+            ret = NEXT_TOKEN(scanner, &token);
+            if (ret != PL_RET_OK) {
+                goto cleanup_name_node;
+            }
+
+            if (token.marker == PL_MARKER_QUESTION) {
+                plAstNode *temp_node;
+
+                temp_node = plAstCreateFamily(PL_MARKER_QUESTION, type_node);
+                if (!temp_node) {
+                    ret = PL_RET_OUT_OF_MEMORY;
+                    goto cleanup_name_node;
+                }
+                memcpy(&temp_node->token, &token, sizeof(token));
+                type_node = temp_node;
+            }
+            else {
+                ret = LOOKAHEAD_STORE(scanner, &token);
+                if (ret != PL_RET_OK) {
+                    goto cleanup_name_node;
+                }
+            }
+        }
+
+        arg_node = plAstCreateFamily(PL_MARKER_COLON, name_node, type_node);
+        if (!arg_node) {
+            ret = PL_RET_OUT_OF_MEMORY;
+            plAstFree(type_node, scanner->table);
+            goto cleanup_name_node;
+        }
+        memcpy(&arg_node->token.location, &location, sizeof(location));
+
+        if (*arg_list) {
+            ret = plAstCreateConnection(PL_MARKER_COMMA, arg_list, arg_node);
+            if (ret != PL_RET_OK) {
+                plAstFree(arg_node, scanner->table);
+                return ret;
+            }
+            memcpy(&(*arg_list)->token.location, &comma_location, sizeof(comma_location));
+        }
+        else {
+            *arg_list = arg_node;
+        }
+    }
+
+cleanup_name_node:
+
+    plAstFree(name_node, scanner->table);
+    return ret;
+}
+
 int
 parseFunction(plLexicalScanner *scanner, plAstNode **node, bool global)
 {
     int ret, function_marker;
     bool declaration;
-    plLexicalLocation function_location, comma_location = {0};
+    plLexicalLocation function_location;
     plLexicalToken token;
     plAstNode *function_name_node = NULL, *arg_list = NULL, *type_node = NULL, *statement_list = NULL;
     plAstMaxSplitNode *splitter;
@@ -60,120 +178,8 @@ parseFunction(plLexicalScanner *scanner, plAstNode **node, bool global)
         goto error;
     }
 
-    while (true) {
-        int previous_marker = scanner->last_marker;
-        plLexicalLocation location;
-        plAstNode *name_node, *arg_node;
-
-        ret = NEXT_TOKEN(scanner, &token);
-        if (ret != PL_RET_OK) {
-            goto error;
-        }
-
-        if (token.marker == PL_MARKER_RIGHT_PARENS) {
-            if (previous_marker == PL_MARKER_COMMA) {
-                PARSER_ERROR("Unexpected ')' following ',' in argument list.");
-                ret = PL_RET_BAD_DATA;
-                goto error;
-            }
-            break;
-        }
-
-        if (token.marker == PL_MARKER_COMMA) {
-            if (previous_marker == PL_MARKER_LEFT_PARENS || previous_marker == PL_MARKER_COMMA) {
-                PARSER_ERROR("Unexpected ',' following %s in argument list.",
-                             plLexicalMarkerName(previous_marker));
-                ret = PL_RET_BAD_DATA;
-                goto error;
-            }
-
-            if (function_marker != PL_MARKER_SOURCE) {
-                PARSER_ERROR("Multiple arguments are only allowed in a SOURCE.");
-                ret = PL_RET_BAD_DATA;
-                goto error;
-            }
-
-            memcpy(&comma_location, &token.location, sizeof(token.location));
-
-            continue;
-        }
-
-        if (token.marker != PL_MARKER_NAME) {
-            PARSER_ERROR("Unexpected %s following %s in argunent list.", plLexicalMarkerName(token.marker),
-                         plLexicalMarkerName(previous_marker));
-            plTokenCleanup(&token, scanner->table);
-            ret = PL_RET_BAD_DATA;
-            goto error;
-        }
-
-        name_node = plAstNew(PL_MARKER_NAME);
-        if (!name_node) {
-            plTokenCleanup(&token, scanner->table);
-            ret = PL_RET_OUT_OF_MEMORY;
-            goto error;
-        }
-        memcpy(&name_node->token, &token, sizeof(token));
-
-        ret = EXPECT_MARKER(scanner, PL_MARKER_COLON, &location);
-        if (ret != PL_RET_OK) {
-            goto cleanup_name_node;
-        }
-
-        ret = parseExtendedType(scanner, &type_node);
-        if (ret != PL_RET_OK) {
-            goto cleanup_name_node;
-        }
-
-        if (type_node->token.marker != PL_MARKER_TYPE) {
-            ret = NEXT_TOKEN(scanner, &token);
-            if (ret != PL_RET_OK) {
-                goto cleanup_name_node;
-            }
-
-            if (token.marker == PL_MARKER_QUESTION) {
-                plAstNode *temp_node;
-
-                temp_node = plAstCreateFamily(PL_MARKER_QUESTION, type_node);
-                if (!temp_node) {
-                    ret = PL_RET_OUT_OF_MEMORY;
-                    goto cleanup_name_node;
-                }
-                memcpy(&temp_node->token, &token, sizeof(token));
-                type_node = temp_node;
-            }
-            else {
-                ret = LOOKAHEAD_STORE(scanner, &token);
-                if (ret != PL_RET_OK) {
-                    goto cleanup_name_node;
-                }
-            }
-        }
-
-        arg_node = plAstCreateFamily(PL_MARKER_COLON, name_node, type_node);
-        if (!arg_node) {
-            ret = PL_RET_OUT_OF_MEMORY;
-            goto cleanup_name_node;
-        }
-        memcpy(&arg_node->token.location, &location, sizeof(location));
-        type_node = NULL;
-
-        if (arg_list) {
-            ret = plAstCreateConnection(PL_MARKER_COMMA, &arg_list, arg_node);
-            if (ret != PL_RET_OK) {
-                plAstFree(arg_node, scanner->table);
-                goto error;
-            }
-            memcpy(&arg_list->token.location, &comma_location, sizeof(comma_location));
-        }
-        else {
-            arg_list = arg_node;
-        }
-
-        continue;
-
-cleanup_name_node:
-
-        plAstFree(name_node, scanner->table);
+    ret = parseArgList(scanner, function_marker, &arg_list);
+    if (ret != PL_RET_OK) {
         goto error;
     }
 
